aaef682c699edb3fa8bfe7e2030309deee4ebeb5
BahareAtaei <ataeibahare1328@gmail.com>
2026-08-17 10:24:27 -0700

    Update step2.py

--- Model_C_C/step2.py
+++ Model_C_C/step2.py
@@ -23,7 +23,7 @@ from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
 
-dff = pd.read_csv('/Users/ershadataei/Documents/Bahare/Programming/Python/Project_Python/ALL EXOPLANETS/RandomForestRegressor/Model_C_C/preprocessed.csv')
+dff = pd.read_csv(r'C:\Users\Sisto\Documents\Programming\Python\Projects\Exoplanets\RandomForestRegressor\Model_C_C\preprocessed.csv')
 df = dff.copy()
 #==============================================
 
@@ -278,4 +278,4 @@ print(filled_mass_dataset.info())
 #=============================================
 
 # save dataset to a new csv file
-filled_mass_dataset.to_csv('/Users/ershadataei/Documents/Bahare/Programming/Python/Project_Python/ALL EXOPLANETS/RandomForestRegressor/Model_C_C/filled_modelC_C.csv', index=False)
+filled_mass_dataset.to_csv(r'C:\Users\Sisto\Documents\Programming\Python\Projects\Exoplanets\RandomForestRegressor\Model_C_C\filled_modelC_C.csv', index=False)
